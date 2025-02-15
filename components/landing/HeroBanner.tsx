8b21b27cb39eb55834a56cfa56389e1928290176
augustomorales92 <augustomorales92@gmail.com>
2025-02-15 13:46:50 +0100

    Refactor HeroBanner component to fetch constructions data and clean up imports

--- components/landing/HeroBanner.tsx
+++ components/landing/HeroBanner.tsx
@@ -1,15 +1,7 @@
-
-"use client"
-
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import cleanEnergy from "@/public/images/constru.jpg"
-import Form from 'next/form'
-import Image from "next/image"
+import cleanEnergy from '@/public/images/constru.jpg'
+import Image from 'next/image'
 import SearchBar from './SearchBar'
-
-
-export default function HeroBanner() {
+import { getConstructions } from '@/actions/constructions'
 
 export default async function HeroBanner() {
   const constructions = await getConstructions()
@@ -24,10 +16,11 @@ export default async function HeroBanner() {
       />
       <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
         <div className="w-full max-w-4xl px-4">
-                  <h1 className="text-4xl md:text-5xl font-bold text-white mb-6 text-center">Busca tu obra</h1>
+          <h1 className="text-4xl md:text-5xl font-bold text-white mb-6 text-center">
+            Busca tu obra
+          </h1>
 
           <SearchBar constructions={constructions} />
-
         </div>
       </div>
     </div>
