12d8f4d009c21f010d765e12da565607e87c61dd
NicoMen <nicomendoza.92@gmail.com>
2025-02-15 11:45:00 +0100

    add prisma

--- components/landing/HeroBanner.tsx
+++ components/landing/HeroBanner.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import cleanEnergy from "@/public/images/clean-energy.jpg"
+import cleanEnergy from "@/public/images/constru.jpg"
 import Form from 'next/form'
 import Image from "next/image"
 
@@ -20,7 +20,7 @@ export default function HeroBanner() {
       />
       <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
         <div className="w-full max-w-4xl px-4">
-          <h1 className="text-4xl md:text-5xl font-bold text-white mb-6 text-center">Descubre Obras Increíbles</h1>
+          <h1 className="text-4xl md:text-5xl font-bold text-white mb-6 text-center">Busca tu obra</h1>
             <Form action='' className="w-full flex gap-2">
             <Input
               type="search"
